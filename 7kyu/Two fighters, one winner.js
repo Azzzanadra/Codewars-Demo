852cfb69f9df474c9a874c8ff9c6bc5223a3ec44
Mohamed Afir <afirmohamedbelkacem@gmail.com>
2022-10-11 23:07:22 +0100

    11/10/2022

---
+++ 7kyu/Two fighters, one winner.js
@@ -0,0 +1,62 @@
+// Create a function that returns the name of the winner in a fight between two fighters.
+
+// Each fighter takes turns attacking the other and whoever kills the other first is victorious. Death is defined as having health <= 0.
+
+// Each fighter will be a Fighter object/instance. See the Fighter class below in your chosen language.
+
+// Both health and damagePerAttack (damage_per_attack for python) will be integers larger than 0. You can mutate the Fighter objects.
+
+// Your function also receives a third argument, a string, with the name of the fighter that attacks first.
+// Example:
+
+//   declare_winner(Fighter("Lew", 10, 2), Fighter("Harry", 5, 4), "Lew") => "Lew"
+  
+//   Lew attacks Harry; Harry now has 3 health.
+//   Harry attacks Lew; Lew now has 6 health.
+//   Lew attacks Harry; Harry now has 1 health.
+//   Harry attacks Lew; Lew now has 2 health.
+//   Lew attacks Harry: Harry now has -1 health and is dead. Lew wins.
+
+// function Fighter(name, health, damagePerAttack) {
+//         this.name = name;
+//         this.health = health;
+//         this.damagePerAttack = damagePerAttack;
+//         this.toString = function() { return this.name; }
+// }
+
+
+//My solution
+function Fighter(name, health, damagePerAttack) {
+    this.name = name;
+    this.health = health;
+    this.damagePerAttack = damagePerAttack;
+    this.toString = function() { return this.name; }
+}
+
+function declareWinner(fighter1, fighter2, firstAttacker) {
+let turn = true;
+if(firstAttacker === fighter1.name){
+turn = true
+}else{
+turn = false
+}
+let str = ''
+while(fighter1.health > 0 && fighter2.health > 0){
+if(turn === true){
+  fighter2.health -= fighter1.damagePerAttack
+  console.log(`${fighter1.name} attacks ${fighter2.name}; ${fighter2.name} now has ${fighter2.health}`)
+  turn = false
+  if(fighter2.health <=0){
+    return fighter1.name
+  }
+}else{
+  fighter1.health -= fighter2.damagePerAttack
+  console.log(`${fighter2.name} attacks ${fighter1.name}; ${fighter1.name} now has ${fighter1.health}`)
+  turn = true
+  if(fighter1.health <=0){
+    return fighter2.name
+  }
+}
+}
+}
+console.log(declareWinner(new Fighter("Harald", 20, 5), new Fighter("Harry", 5, 4), "Harald"))
